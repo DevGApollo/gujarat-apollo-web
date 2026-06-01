48e23e84968094b85010078ff646b56a58659466
it_dev <it_dev@gapollo.net>
2026-06-01 14:00:58 +0530

    fix: added fevicon icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
+
 // Global Components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
